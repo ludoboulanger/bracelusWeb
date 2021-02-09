e71bb4350d042cd11c20c50ca90ac0a41f15de2b
lboulanger <ludovic.boulanger@outlook.com>
2021-02-09 16:36:49 -0500

    J<ai ajouter uen requete et jai changer un peu les couleur

--- BracelUSWEB/src/component/MainTitle.tsx
+++ BracelUSWEB/src/component/MainTitle.tsx
@@ -8,10 +8,9 @@ var ReactDOM = require('react-dom');
 const Title = Styled.h1`
   font-size: 100px;
   width: 90%;
-  background-color: #00e600;
   text-align: center;
   margin: 25px;
-  border: 2px solid black;
+  color: #ffffff;
 `;
 
 export default class MainTitle extends React.Component {
