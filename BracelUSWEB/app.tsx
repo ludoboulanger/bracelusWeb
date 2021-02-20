b3ec667373dd29aa107a02429417f1bad6d2f52f
Master_Cram <49244480+MarcSimardUsherbrooke@users.noreply.github.com>
2021-02-20 16:26:02 -0500

    Fix ajout des route et import

--- BracelUSWEB/app.tsx
+++ BracelUSWEB/app.tsx
@@ -1,5 +1,8 @@
 import MainTitle from './src/component/MainTitle';
 import Acceuil from './src/component/Acceuil';
+import ActivitePhysique from './src/component/ActivitePhysique';
+import EtatDeSommeil from './src/component/EtatDeSommeil';
+import Controle from './src/component/Controle';
 import Styled from 'styled-components';
 import Menu from './src/component/Menu';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -35,6 +38,9 @@ export class PageDemo1 extends React.Component {
                             <MenuColumn><Menu /></MenuColumn>
                             <td>
                                 <Route path="/index.html" component={Acceuil} />
+                                <Route path="/ActivitePhysique.html" component={ActivitePhysique} />
+                                <Route path="/EtatDeSommeil.html" component={EtatDeSommeil} />
+                                <Route path="/Controle.html" component={Controle} />
                             </td>
                         </tr>
                     </Table>
